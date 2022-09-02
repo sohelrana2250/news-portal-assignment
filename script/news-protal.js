0208bdce0e29fcf8e4ef0f8855459d6431bde907
sohelrana <amsr215019@gmail.com>
2022-09-03 03:36:57 +0600

    third-commit

--- script/news-protal.js
+++ script/news-protal.js
@@ -1,5 +1,5 @@
 
-
+// 
 const loadNewsprotalApi = async () => {
     try {
 
@@ -14,6 +14,9 @@ const loadNewsprotalApi = async () => {
     }
 }
 
+
+
+// display catagory of news 
 const displayNews = (data) => {
 
     const navigationList = document.getElementById('navigation-list');
@@ -30,14 +33,128 @@ const displayNews = (data) => {
         navigationList.appendChild(newList);
 
 
-        console.log(x);
+        //console.log(x);
 
     })
 
 
 }
-
+// identify the  news id number
 const handelNewID = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+
+    fetch(url).then((res) => res.json()).then((data) => displayNewsMaterial(data.data)).catch((error) => console.log(error.message))
+
+
+}
+//display News Materials
+const displayNewsMaterial = (newData) => {
+    //console.log(newData)
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerText = ''
+
+    newData.forEach((v) => {
+
+        const newContainer = document.createElement('div');
+        newContainer.innerHTML = `
+         
+         <div class="row g-0 mb-3">
+         <div class="col-md-4">
+             <img src="${v.image_url}" class="img-fluid rounded-start" alt="...">
+         </div>
+         <div class="col-md-8">
+             <div class="card-body">
+                 <h4 class="card-title">${v.title}</h4>
+                 <p>${v.details.slice(0, 300).concat('...')}</p>
+
+
+    <div class="row">
+
+        <div class=" col-lg-3 col-sm-6 mt-5">
+ 
+        
+        <div class="d-flex">
+        <img src="${v.author.img}" class="rounded-circle w-25 h-25" alt="...">
+        
+        <div class="m-3"> <p>${v.author.name}</p>
+        <p >${v.author.published_date}</p></div>
+        
+        </div>
+        
+        
+        
+        </div>
+     
+        <div class=" col-lg-3 col-sm-6 mt-5 d-flex">
+            
+        <p class="m-2"><i class="fa-regular fa-eye fs-3 "></i></p>
+
+        <h6 class="m-3">${v.total_view}</h6>
+
+
+        </div>
+        <div class=" col-lg-3 col-sm-6 mt-5">
+
+         <div class="text-center">
+         <i class="fa-solid fa-star-half-stroke fs-3 text-warning p-2"></i>
+         <i class="fa-regular fa-star text-warning p-2 fs-3"></i>
+         <i class="fa-regular fa-star text-warning p-2 fs-3"></i>
+         <i class="fa-regular fa-star text-warning p-2 fs-3"></i>
+         <i class="fa-regular fa-star text-warning p-2 fs-3"></i>
+         </div>
+    
+    
+
+        </div>
+        <div class=" col-lg-3 col-sm-6 mt-5 text-center">
+       
+        <button data-bs-toggle="modal" data-bs-target="#exampleModal"  onclick="handelNewDetails('${v._id}')" type="button" class="btn btn-outline-danger"><i class="fa-solid fa-arrow-right"></i></button>
+       
+        </div>
+
+             </div>
+         </div>
+         `
+        //data-bs-toggle="modal" data-bs-target="#exampleModal"
+        newsContainer.appendChild(newContainer)
+    })
+
+
+
+
+}
+// handel new details 
+const handelNewDetails = async (newDetailsId) => {
+
+    const url = ` https://openapi.programming-hero.com/api/news/${newDetailsId}`
+    const res = await fetch(url);
+    const data = await res.json();
+
+    displayMoreDetails(data.data)
+
+
+}
+const displayMoreDetails = (moreData) => {
+    console.log(moreData)
+    const modalTatile = document.getElementById('exampleModalLabel');
+    const newsDetails = document.getElementById('news-details');
+
+    moreData.forEach((v) => {
+        modalTatile.innerText = `${v.title}`
+        newsDetails.innerHTML = `
+        <div class="card" style="width: 28rem;">
+  <img src="${v ? v.thumbnail_url : 'Picture does not exist API'}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p> Budget :  ${v.rating.badge}</p>
+    <p>number : 
+     ${v.rating.number}</p>
+  </div>
+</div>
+        
+        `
+
+    })
 
 
 
